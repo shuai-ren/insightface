84cf11dc9a7a09671d4177262c2bd60905ffc8d2
任帅 <renshuai@jylink.com>
2024-08-16 20:57:11 +0800

    GPU解码版本

---
+++ face_cudacodec.py
@@ -0,0 +1,159 @@
+import os
+os.environ["NO_ALBUMENTATIONS_UPDATE"] = '1'
+import argparse
+import datetime
+import time
+import pickle
+from collections import deque
+from PIL import Image, ImageDraw, ImageFont
+
+import cv2
+import faiss
+import numpy as np
+from insightface.app import FaceAnalysis
+from stream import start
+
+
+def load_index_and_metadata(index_file, metadata_file):
+    index = faiss.read_index(index_file)
+    with open(metadata_file, 'rb') as f:
+        features_dict = pickle.load(f)
+    return index, features_dict
+
+
+def normalize_vector(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+
+def main():
+    app = FaceAnalysis(name='buffalo_l', root='./', allowed_modules=['detection', 'recognition'], providers=['CUDAExecutionProvider'])
+    app.prepare(ctx_id=0, det_size=(640, 640), det_thresh=args.conf_thres)
+
+    index, features_dict = load_index_and_metadata(args.index_file, args.metadata_file)
+    font = ImageFont.truetype(args.font_file, 50)
+
+    data_deque = deque(maxlen=100)
+    start(data_deque, args.http_port, args.rtsp_port, out_size=(out_width, out_height))
+
+    param = cv2.cudacodec.VideoReaderInitParams()
+    param.udpSource = True
+    video_reader = cv2.cudacodec.createVideoReader(args.video, params=param)
+    stream = cv2.cuda_Stream()
+    color = (0, 0, 255)
+
+    frame_id = 0
+    reconnected = False
+    objects = []
+
+    start_time = time.time()
+    while True:
+        t0 = time.time()
+        ret, frame_gpu = video_reader.nextFrame()
+        t1 = time.time()
+        if not ret or frame_gpu is None:
+            if reconnected:
+                break
+            video_reader = cv2.cudacodec.createVideoReader(args.video, params=param)
+            reconnected = True
+            continue
+        
+        frame_bgr_gpu = cv2.cuda.cvtColor(frame_gpu, cv2.COLOR_BGRA2BGR, stream=stream)
+        frame_bgr_gpu_outsize = cv2.cuda.resize(frame_bgr_gpu, (out_width, out_height), stream=stream)
+        frame = frame_bgr_gpu_outsize.download(stream=stream)
+        orig_w, orig_h = frame_bgr_gpu.size()
+
+        frame_id += 1
+
+        if frame_id % args.interval == 0:
+
+            new_width = 640
+            new_height = int(new_width * out_width / out_height)
+            resized_frame = cv2.cuda.resize(frame_bgr_gpu, (new_width, new_height), stream=stream).download(stream=stream)
+
+            objects = []
+            faces = app.get(resized_frame)
+
+            for face in faces:
+                normalized_features = normalize_vector(face["embedding"])
+                conf = face["det_score"].tolist()
+                bbox = face.bbox / (new_width, new_height)
+                bbox_orig = (bbox * (orig_w, orig_h)).astype(int).tolist()
+                bbox_draw = (bbox * (out_width, out_height)).astype(int)
+                distances, indices = index.search(np.array([normalized_features]), 1)
+                person_name = "未知"
+ 
+                distance = distances[0][0].tolist()
+                if distance < args.dist_thres:
+                    for n, i in features_dict.items():
+                        if indices[0][0] in i:
+                            person_name = n
+                            break
+
+                obj = {
+                    "name": person_name,
+                    "distance": distance,
+                    "confidence": conf,
+                    "face_coordinates": bbox_orig
+                }
+
+                objects.append(obj)
+
+                image_pil = Image.fromarray(frame)
+                draw = ImageDraw.Draw(image_pil)
+                text = "{}: {:.2f}".format(person_name, distance)
+                draw.text((bbox_draw[0], bbox_draw[1] - 50), text, font=font, fill=color)
+                frame = np.array(image_pil)
+                cv2.rectangle(frame, (bbox_draw[0], bbox_draw[1]), (bbox_draw[2], bbox_draw[3]), color, 2)
+
+            print('fps: %.3f, \tavg_fps: %.3f, \tbuffer: %g' % (1 / (time.time() - t0), frame_id / (time.time() - start_time), len(data_deque)))
+
+        json = {"frame_id": frame_id,
+                "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                # "resolution": [w, h],
+                "objects": objects
+        }
+        data_deque.append((json, frame))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', type=str, required=True, help='video url')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--dist-thres', type=float, default=1.0, help='distance threshold')
+    parser.add_argument('--interval', type=int, default=5, help='sample interval')
+    parser.add_argument('--http_port', type=int, required=True, help='http port')
+    parser.add_argument('--rtsp_port', type=int, required=True, help='rtsp port')
+    parser.add_argument('--index-file', type=str, default="./data/faiss_index.index", help='index file')
+    parser.add_argument('--metadata-file', type=str, default="./data/metadata.pkl", help='metadata file')
+    parser.add_argument('--font-file', type=str, default="./font/simsun.ttc", help='font file')
+
+    args = parser.parse_args()
+    print(args)
+
+    import json
+
+    cwd = os.getcwd()[7:]
+    business_json_list = ["business.json", "/root/MBAB/AI/{}/etc/business.json".format(cwd)]
+    out_width = 1280
+    out_height = 720
+    add_line = lines = color = width = None
+    for business_json in business_json_list:
+        if os.path.isfile(business_json):
+            business = json.load(open(business_json))
+            params = business["business_params"]
+            out_size = params.get("out_size")
+            if out_size is not None:
+                out_width = out_size.get("out_width")
+                out_height = out_size.get("out_height")
+                
+            add_line = params.get("add_line")
+            if add_line is not None:
+                lines = add_line.get("lines")
+                width = add_line.get("width")
+                color = add_line.get("color")
+            break
+
+    main()
--- stream.py
+++ stream.py
@@ -19,7 +19,10 @@ class ClientManager:
     def add_client(self):
         with self.condition:
             client_id = str(uuid.uuid4())
-            self.clients[client_id] = {"json_queue": deque(maxlen=self.max_length)}
+            self.clients[client_id] = {
+                "json_queue": deque(maxlen=self.max_length),
+                "frame_queue": deque(maxlen=self.max_length)
+            }
             print("new client: ", client_id)
             print(
                 "current clients [{}]:\n {}".format(
@@ -45,15 +48,28 @@ class ClientManager:
                 client["json_queue"].append(json_data)
             self.condition.notify_all()
 
+    def update_frame(self, frame_data):
+        with self.condition:
+            for client in self.clients.values():
+                client["frame_queue"].append(frame_data)
+            self.condition.notify_all()
+
     def get_json(self, client_id):
         with self.condition:
             while not self.clients[client_id]["json_queue"]:
                 self.condition.wait()
             return self.clients[client_id]["json_queue"].popleft()
 
+    def get_frame(self, client_id):
+        with self.condition:
+            while not self.clients[client_id]["frame_queue"]:
+                self.condition.wait()
+            return self.clients[client_id]["frame_queue"].popleft()
+
 
 client_manager = ClientManager()
-app_json = Flask(__name__)
+app_json = Flask("json_server")
+app_mjpeg = Flask("mjpeg_server")
 
 
 @app_json.route("/")
@@ -71,6 +87,23 @@ def json_endpoint():
     return Response(generate_json(client_id), content_type="text/event-stream; charset=utf-8")
 
 
+@app_mjpeg.route("/")
+def mjpeg_endpoint():
+    client_id = client_manager.add_client()
+
+    def generate_mjpeg(client_id):
+        try:
+            while True:
+                frame_data = client_manager.get_frame(client_id)
+                yield frame_data
+        finally:
+            client_manager.remove_client(client_id)
+
+    response = Response(generate_mjpeg(
+        client_id), content_type="multipart/x-mixed-replace; boundary=FRAME")
+    return response
+
+
 def replace_yaml_field(filename, field, value):
     try:
         with open(filename, "r") as file:
@@ -82,20 +115,49 @@ def replace_yaml_field(filename, field, value):
         print(f"{filename} not found.")
 
 
-def start_http_server(json_deque, port):
-    def http_server_thread():
+def start_json_server(json_deque, json_port):
+    def json_server_thread():
         while True:
             try:
                 json_data = json_deque.popleft()
-                json_data_str = json.dumps(json_data, ensure_ascii=False) + "\n"
+                json_data_str = json.dumps(
+                    json_data, ensure_ascii=False) + "\n"
                 json_data_bytes = json_data_str.encode("UTF-8")
                 client_manager.update_json(json_data_bytes)
             except IndexError:
-                time.sleep(0.01)
+                time.sleep(0.001)
 
-    threading.Thread(target=http_server_thread, daemon=True).start()
+    threading.Thread(target=json_server_thread, daemon=True).start()
     threading.Thread(
-        target=app_json.run, kwargs={"host": "0.0.0.0", "port": port}, daemon=True
+        target=app_json.run, kwargs={"host": "0.0.0.0", "port": json_port}, daemon=True
+    ).start()
+
+
+def start_mjpeg_server(frame_deque, mjpeg_port, size=None, quality=50):
+    def mjpeg_server_thread():
+        while True:
+            try:
+                frame = frame_deque.popleft()
+                if size:
+                    frame = cv2.resize(frame, size)
+                _, jpg_str = cv2.imencode(
+                    ".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+                frame_data = (
+                    b"--FRAME\r\n"
+                    b"Content-Type: image/jpeg\r\n"
+                    b"Content-Length: "
+                    + str(len(jpg_str)).encode()
+                    + b"\r\n\r\n"
+                    + jpg_str.tobytes()
+                    + b"\r\n"
+                )
+                client_manager.update_frame(frame_data)
+            except IndexError:
+                time.sleep(0.001)
+
+    threading.Thread(target=mjpeg_server_thread, daemon=True).start()
+    threading.Thread(
+        target=app_mjpeg.run, kwargs={"host": "0.0.0.0", "port": mjpeg_port}, daemon=True
     ).start()
 
 
@@ -103,7 +165,8 @@ def create_mediamtx_process(rtsp_port):
     mediamtx_executable = "./mediamtx/mediamtx"
     mediamtx_config = "./mediamtx/mediamtx.yml"
     replace_yaml_field(mediamtx_config, "rtspAddress", f":{rtsp_port}")
-    mediamtx_process = subprocess.Popen([mediamtx_executable, mediamtx_config], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    mediamtx_process = subprocess.Popen(
+        [mediamtx_executable, mediamtx_config], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return mediamtx_process
 
 
@@ -115,9 +178,8 @@ def create_ffmpeg_process(rtsp_port, fps, orig_size, out_size, bitrate):
         "-s", "{}x{}".format(*orig_size),  # 原始图像尺寸
         "-r", str(fps),
         "-i", "-",
-        "-vf", "scale={}:{}".format(*out_size),  # 调整尺寸
         "-c:v", "libx264",
-        "-preset", "veryfast",
+        "-preset", "ultrafast",
         "-tune", "zerolatency",
         "-b:v", "{}k".format(bitrate),  # 视频比特率
         "-maxrate", "{}k".format(bitrate),  # 最大比特率
@@ -128,17 +190,24 @@ def create_ffmpeg_process(rtsp_port, fps, orig_size, out_size, bitrate):
         "-rtsp_transport", "tcp",
         "rtsp://127.0.0.1:{}/rtsp".format(rtsp_port),
     ]
-    ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    if out_size is not None:
+        ffmpeg_command.insert(11, "-vf")
+        ffmpeg_command.insert(12, "scale={}:{}".format(*out_size))
+
+    ffmpeg_process = subprocess.Popen(
+        ffmpeg_command, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return ffmpeg_process
 
 
 def process_video(frame_deque, rtsp_port, out_size, fps, bitrate, timeout):
     while not frame_deque:
-        time.sleep(0.01)
+        time.sleep(0.001)
     frame = frame_deque.popleft()
     height, width, _ = frame.shape
     mediamtx_process = create_mediamtx_process(rtsp_port)
-    ffmpeg_process = create_ffmpeg_process(rtsp_port, fps, (width, height), out_size, bitrate)
+    ffmpeg_process = create_ffmpeg_process(
+        rtsp_port, fps, (width, height), out_size, bitrate)
     check_ffmpeg_time = time.time()
     last_data_time = time.time()
 
@@ -150,7 +219,8 @@ def process_video(frame_deque, rtsp_port, out_size, fps, bitrate, timeout):
                     print("ffmpeg process terminated, restarting...")
                     ffmpeg_process.stdin.close()
                     ffmpeg_process.terminate()
-                    ffmpeg_process = create_ffmpeg_process(rtsp_port, fps, (width, height), out_size)
+                    ffmpeg_process = create_ffmpeg_process(
+                        rtsp_port, fps, (width, height), out_size)
 
             try:
                 frame = frame_deque.popleft()
@@ -159,9 +229,10 @@ def process_video(frame_deque, rtsp_port, out_size, fps, bitrate, timeout):
                 last_data_time = time.time()
             except IndexError:
                 if time.time() - last_data_time > timeout:
-                    print("No new data received for specified timeout period. Exiting...")
+                    print(
+                        "No new data received for specified timeout period. Exiting...")
                     break
-                time.sleep(0.01)
+                time.sleep(0.001)
             except BrokenPipeError as e:
                 print(f"BrokenPipeError: {e}")
 
@@ -171,7 +242,7 @@ def process_video(frame_deque, rtsp_port, out_size, fps, bitrate, timeout):
         ffmpeg_process.terminate()
 
 
-def start(data_deque, http_port, rtsp_port, out_size=(640, 360), fps=25, bitrate=2000, max_length=100, timeout=10):
+def start(data_deque, json_port, rtsp_port=None, mjpeg_port=None, quality=50, out_size=None, fps=25, bitrate=2000, max_length=100, timeout=10):
     frame_deque = deque(maxlen=max_length)
     json_deque = deque(maxlen=max_length)
 
@@ -182,18 +253,24 @@ def start(data_deque, http_port, rtsp_port, out_size=(640, 360), fps=25, bitrate
                 json_deque.append(data[0])
                 frame_deque.append(data[1])
             except IndexError:
-                time.sleep(0.01)
+                time.sleep(0.001)
 
     threading.Thread(target=split_data, daemon=True).start()
-    threading.Thread(
-        target=process_video,
-        args=(frame_deque, rtsp_port, out_size, fps, bitrate, timeout),
-        daemon=True,
-    ).start()
-    start_http_server(json_deque, http_port)
+    if mjpeg_port:
+        start_mjpeg_server(frame_deque, mjpeg_port, out_size, quality)
+    else:
+        threading.Thread(
+            target=process_video,
+            args=(frame_deque, rtsp_port, out_size, fps, bitrate, timeout),
+            daemon=True,
+        ).start()
+    start_json_server(json_deque, json_port)
 
-    print("json url: http://127.0.0.1:{}".format(http_port))
-    print("rtsp url: rtsp://127.0.0.1:{}/rtsp".format(rtsp_port))
+    print("json url: http://127.0.0.1:{}".format(json_port))
+    if mjpeg_port:
+        print("mjpeg url: http://127.0.0.1:{}".format(mjpeg_port))
+    else:
+        print("rtsp url: rtsp://127.0.0.1:{}/rtsp".format(rtsp_port))
 
 
 if __name__ == "__main__":
@@ -201,9 +278,11 @@ if __name__ == "__main__":
     import datetime
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--video", required=True, type=str, help="video file location")
-    parser.add_argument("--http_port", type=int, default=8553, help="port to stream json")
-    parser.add_argument("--rtsp_port", type=int, default=8554, help="port to stream video")
+    parser.add_argument("--video", required=True, type=str,
+                        help="video file location")
+    parser.add_argument("--json_port", type=int, help="json port")
+    parser.add_argument("--rtsp_port", type=int, help="rtsp port ")
+    parser.add_argument("--mjpeg_port", type=int, help="mjpeg port")
     args = parser.parse_args()
 
     data_deque = deque(maxlen=100)
@@ -224,5 +303,5 @@ if __name__ == "__main__":
             data_deque.append([json_data, frame])
         cap.release()
 
-    start(data_deque, args.http_port, args.rtsp_port)
+    start(data_deque, args.json_port, args.rtsp_port)
     read_video(args.video, data_deque)
